8a834a243b91a36eac33407652f1cefdab03f5fe
Alister Brumley <109202180+AstaBrum@users.noreply.github.com>
2024-01-10 04:19:14 +1100

    completed execercise 02
    checked and completed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
+const repeatString = function (string, num) {
 
+    if (num < 0) {
+        return "ERROR"
+    }
+    
+    let returnValue = "";
+
+    for (let runs = 0; runs < num; runs++) {
+        returnValue = returnValue + string;
+    }
+
+    return returnValue;
 };
 
 // Do not edit below this line
