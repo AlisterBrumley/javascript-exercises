5130c660f7ef16f711b03f112c73f3046e2eab3c
Alister Brumley <109202180+AstaBrum@users.noreply.github.com>
2024-01-10 04:56:54 +1100

    completed exercise 04
    completed and checked

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...toRemove) {
+    for (const element of toRemove){
+        const index = array.indexOf(element)
+        if (index > -1){
+            array.splice(index, 1);
+        }
+    }
 
+    return array;
 };
 
 // Do not edit below this line
