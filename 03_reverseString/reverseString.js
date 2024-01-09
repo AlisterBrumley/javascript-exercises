8637462387c4644ffcfdeef400d9cf1dfea4d60d
Alister Brumley <109202180+AstaBrum@users.noreply.github.com>
2024-01-10 04:40:53 +1100

    completed exercise 03
    checked and completed

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function(str) {
+    const strLen = str.length;
+    let strRet = "";
 
+    for (let letters = 0; letters < strLen; letters++) {
+        strRet = str[letters].concat(strRet);
+    }
+
+    return strRet;
 };
 
 // Do not edit below this line
