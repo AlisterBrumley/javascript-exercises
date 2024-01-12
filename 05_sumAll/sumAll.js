d5bc5cbcc70948899ae43d2d899ad967f6daedee
Alister Brumley <109202180+AstaBrum@users.noreply.github.com>
2024-01-12 15:54:20 +1100

    completed execerise 5
    checked and completed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,27 @@
-const sumAll = function() {
+const sumAll = function (firstNum, secNum) {
+    let sum = 0;
+    let adder;
+    let total;
+
+    if (!Number.isInteger(firstNum) || !Number.isInteger(secNum)) {
+        return "ERROR";
+    } else if (firstNum < 0 || secNum < 0) {
+        return "ERROR";
+    } else if (firstNum > secNum) {
+        adder = secNum;
+        total = firstNum;
+    } else {
+        adder = firstNum;
+        total = secNum;
+    }
+
+
+    do {
+        sum = sum + adder;
+        adder++;
+    } while (adder <= total)
+
+    return sum;
 
 };
 
