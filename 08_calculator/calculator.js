fab29a864957952c6ca85b568746b81cb526637c
Alister Brumley <109202180+AlisterBrumley@users.noreply.github.com>
2024-02-26 18:01:12 +1100

    completed and checked exercise 8
    theres a better way to do factorial using recursion, but i accidentally learned it by googling so made my own

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,42 @@
-const add = function() {
-	
+const add = function(first, second) {
+  return first + second;
 };
 
-const subtract = function() {
-	
+const subtract = function(first, second) {
+  return first - second;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  return array.reduce((total, number) => {
+    return total + number;
+  }, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((total, number) => {
+    if (!total) {
+      return number;
+    } else {
+      return total * number;
+    }
+  });
 };
 
-const power = function() {
-	
+const power = function(first, second) {
+  return first ** second
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+  if (number == 0) {
+    return 1;
+  }
+
+  let fSum = 1;
+  for (let i = 1; i <= number; i++) {
+    fSum = fSum * i;
+  }
+
+  return fSum
 };
 
 // Do not edit below this line
