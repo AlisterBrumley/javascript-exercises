54deabbb948a2aa266d3476c9d0cc9ee0849ad44
Alister Brumley <109202180+AlisterBrumley@users.noreply.github.com>
2024-02-26 23:42:25 +1100

    completed exercise 9
    checked and completed

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
+const palindromes = function (fullStr) {
+    forwardTemp = fullStr.replaceAll(/[^\w\s\']/g, "").replaceAll(/\s/g, "").toLowerCase();
+    backwardTemp = forwardTemp.split("").reverse().join("");
 
+    if (forwardTemp != backwardTemp) {
+        return false
+    } else {
+        return true
+    }
 };
 
 // Do not edit below this line
