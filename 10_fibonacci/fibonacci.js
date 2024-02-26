4d6ddf592f4e322c4bd65b4aff43744fe095eebb
Alister Brumley <109202180+AlisterBrumley@users.noreply.github.com>
2024-02-27 01:08:30 +1100

    checked and completed exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,25 @@
-const fibonacci = function() {
+const fibonacci = function(fIndex) {
 
+    // input management
+    if (fIndex < 0) {
+        return "OOPS";
+    } else if (fIndex == 0) {
+        return 0;
+    }
+
+    // setting vars
+    let fMinus = 1;
+    let fMinusMinus = 0;
+    let fNumber = 1;
+
+    // looping to create fib numbers
+    for (let i = 1; i < fIndex; i++) {
+        fNumber = fMinus + fMinusMinus;
+        fMinusMinus = fMinus;
+        fMinus = fNumber;
+    }
+    
+    return fNumber;
 };
 
 // Do not edit below this line
