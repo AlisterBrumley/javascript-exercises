c0a5be3244725d0ae13edd777df0a3ead3d91bb6
Alister Brumley <109202180+AlisterBrumley@users.noreply.github.com>
2024-02-27 13:32:31 +1100

    completed and checked exercise 12
    actually did it in less lines then they did

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,11 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+    return people.reduce((oldest, person) => {
+        oldest.yearOfDeath = !oldest.yearOfDeath ? new Date().getFullYear() : oldest.yearOfDeath
 
+        let oldAge = oldest.yearOfDeath - oldest.yearOfBirth;
+        let personAge = person.yearOfDeath - person.yearOfBirth;
+        return oldest = oldAge < personAge ? person : oldest;
+    });
 };
 
 // Do not edit below this line
